82f903e9c23d5a33171229c9676814aff8cd1527
pgmnt <mchawyotha@gmail.com>
2022-10-31 21:23:32 +0700

    add register

--- src/route/RouteProfile.ts
+++ src/route/RouteProfile.ts
@@ -25,3 +25,21 @@ export class RouteProfile extends Route {
         });
     }
 }
+
+export class RouteRegister extends Route {
+    setup(express: Application, server: Server): void {
+        express.post(server.relativePath("profile"), async (req, res) => {
+            let session: Session = res.locals.session;
+            await server.sessionManager.createUser(session);
+                req.send(this.serialize({
+                    success: true,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    password: req.body.password,
+                    gender: req.body.gender,
+                    dob: req.body.dob,
+                }));
+                return;
+            }
+        });
+    }
