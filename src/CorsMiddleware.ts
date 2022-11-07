a26b2c908a3b9d1ba9305aa9b89318d060f318ee
porama <porama@porama.dev>
2022-11-07 23:19:01 +0700

    fix CORS

--- src/CorsMiddleware.ts
+++ src/CorsMiddleware.ts
@@ -2,7 +2,14 @@ import {Request, Response, NextFunction} from 'express';
 
 export default (req: Request, res: Response, next: NextFunction) => {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Credentials', "true");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', "Content-Type");
-    next();
+    res.header('Access-Control-Allow-Credentials', "true");
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+        res.send();
+    } else {
+        next();
+    }
 }
