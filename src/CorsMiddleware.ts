c763e37e9b9cc1128ac0b9d0f548664ba0dea9a6
pgmnt <mchawyotha@gmail.com>
2022-11-09 18:31:25 +0700

    added patch

--- src/CorsMiddleware.ts
+++ src/CorsMiddleware.ts
@@ -1,12 +1,12 @@
-import {Request, Response, NextFunction} from 'express';
+import {Request, Response, NextFunction} from "express";
 
 export default (req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', "Content-Type");
-    res.header('Access-Control-Allow-Credentials', "true");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Credentials", "true");
 
-    if (req.method === 'OPTIONS') {
+    if (req.method === "OPTIONS") {
         res.sendStatus(200);
         res.send();
     } else {
